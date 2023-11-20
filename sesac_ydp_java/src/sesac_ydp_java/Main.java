99cf8928dd66d45e2b42d5e23315fe2e16b506ee
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-20 14:36:23 +0900

    feat: 레포 생

---
+++ sesac_ydp_java/src/sesac_ydp_java/Main.java
@@ -0,0 +1,15 @@
+package sesac_ydp_java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("안녕하세요");
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("i = " + i);
+		}
+	}
+
+}
